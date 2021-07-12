46b3ce26c512cad5a445829ca8b4543b98bb88a2
guptaggar <56382406+guptaggar@users.noreply.github.com>
2021-07-12 10:29:36 +0530

    Update post.php

--- views/post.php
+++ views/post.php
@@ -4,6 +4,6 @@ if(isset($_SESSION['name'])){
     $text = $_POST['text'];
      
     $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
-    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+    file_put_contents("views\log.html", $text_message, FILE_APPEND | LOCK_EX);
 }
 ?>
